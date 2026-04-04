35983d0ab4274526f981d54f28b5697e2f438f7d
EmyDuf <53259045+EmyDuf@users.noreply.github.com>
2026-04-04 23:15:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,25 @@ st.set_page_config(layout="wide",    page_title='Wellness : fr',
     page_icon=':smile:',initial_sidebar_state='collapsed',) # This is an emoji shortcode. Could be a URL too.)
 #@m = folium.Map(location=[43.59966,1.44043,], zoom_start=11, tiles='OpenStreetMap')
 #folium_static(m)
+
+# Primary accent for interactive elements
+primaryColor = '#d33682'
+# Background color for the main content area
+backgroundColor = '#002b36'
+# Background color for sidebar and most interactive widgets
+secondaryBackgroundColor = '#586e75'
+# Color used for almost all text
+textColor = '#fafafa'
+# Font family for all text in the app, except code blocks
+# Accepted values (serif | sans serif | monospace) 
+# Default: "sans serif"
+font = "sans serif"
+
+# -----------------------------------------------------------------------------
+#@st.cache_data
+def get_data_station():
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/csv_long/bien_etre.csv'
+    df = pd.read_csv(DATA_DIR, sep=';', encoding='utf-8')
+    df['Valeur_Mesurée'] = df['Valeur_Mesurée'].str.replace(",", ".", regex=False)  # replace decimal comma with dot
+    df['Valeur_Mesurée'] = df['Valeur_Mesurée'].astype(float)
