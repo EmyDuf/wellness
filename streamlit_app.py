61494ce5bc9e82349d145262edd35dfc3a8f498f
EmyDuf <53259045+EmyDuf@users.noreply.github.com>
2026-04-07 20:19:34 +0000

    size

--- streamlit_app.py
+++ streamlit_app.py
@@ -341,8 +341,8 @@ with tab1:
                         xref="x", yref="y",
                         xanchor="center", yanchor="middle",
                         x=row["Mesure"], y=row["Valeur_Mesurée"],
-                        sizex=7,#maxi * 0.2, #row["Valeur_Mesurée"]/5, #np.sqrt(row["pop"] / df["pop"].max()) * maxi * 0.2 + maxi * 0.05,
-                        sizey=7,#maxi * 0.2, #row["Valeur_Mesurée"]/5, #np.sqrt(row["pop"] / df["pop"].max()) * maxi * 0.2 + maxi * 0.05,
+                        sizex=2,#maxi * 0.2, #row["Valeur_Mesurée"]/5, #np.sqrt(row["pop"] / df["pop"].max()) * maxi * 0.2 + maxi * 0.05,
+                        sizey=2,#maxi * 0.2, #row["Valeur_Mesurée"]/5, #np.sqrt(row["pop"] / df["pop"].max()) * maxi * 0.2 + maxi * 0.05,
                         sizing="contain", opacity=0.8, layer="above"
                     )
                 )
